daa4d833c38027feee902929132ee7691529cd2e
Konrad <krobaszynski@edu.cdv.pl>
2018-11-26 21:01:40 +0100

    Spring Boot app init

---
+++ src/main/java/com/konradmikolaj/boardgamelibrarymanager/BoardgamelibrarymanagerApplication.java
@@ -0,0 +1,12 @@
+package com.konradmikolaj.boardgamelibrarymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardgamelibrarymanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardgamelibrarymanagerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/konradmikolaj/boardgamelibrarymanager/BoardgamelibrarymanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.konradmikolaj.boardgamelibrarymanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoardgamelibrarymanagerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
