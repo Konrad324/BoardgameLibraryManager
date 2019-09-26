1c4afa2153b502490620df262a8793c9ebd38cf4
Konrad <krobaszynski@edu.cdv.pl>
2019-09-26 22:37:35 +0200

    dluzsze opisy gier

--- src/main/java/com/konradmikolaj/boardgamelibrarymanager/services/DemoContentCreator.java
+++ src/main/java/com/konradmikolaj/boardgamelibrarymanager/services/DemoContentCreator.java
@@ -49,16 +49,24 @@ public class DemoContentCreator {
     }
 
     private void createBoardGames() {
-        BoardGame boardGame1 = BoardGame.of(USER_1, "Osadnicy z Catanu", "Ekonomiczna", "Dom");
-        BoardGame boardGame2 = BoardGame.of(USER_1, "Domek", "Lekka gra rodzinna", USER_1);
-        BoardGame boardGame3 = BoardGame.of(USER_2, "Splendor", "Mózgożerna ekonomia", "Dom");
-        BoardGame boardGame4 = BoardGame.of(USER_2, "Magia i Miecz", "Klasyczny losowy RPG", USER_2);
-        BoardGame boardGame5 = BoardGame.of(USER_2, "Robinson Crusoe", "Kooperacyjna, przetrwanie", "Dom");
+        BoardGame boardGame1 = BoardGame.of(USER_1, "Osadnicy z Catanu", "Gracze są osadnikami na niedawno odkrytej wyspie Catan. Każdy z nich przewodzi świeżo założonej kolonii i rozbudowuje ją stawiając na dostępnych obszarach nowe drogi i miasta. Każda kolonia zbiera dostępne dobra naturalne.", "Dom");
+        BoardGame boardGame2 = BoardGame.of(USER_1, "Domek", "W ciągu dwunastu rund zbierzesz tyle kart, by zapełnić swoją planszę pokojami, wybrać kolor dachu i znaleźć miejsce na stylowe lub praktyczne dodatki, tworząc tym samym niepowtarzalny >Domek< z charakterem", USER_1);
+        BoardGame boardGame3 = BoardGame.of(USER_2, "Splendor", "Gracze wcielają się w renesansowych kupców, którzy próbują nabyć kopalnie klejnotów, środki transportu, sklepy - wszystko to w celu zdobycia jak największego prestiżu.", "Dom");
+        BoardGame boardGame4 = BoardGame.of(USER_2, "Magia i Miecz", "W tej pełnej przygód grze wyruszysz na niebezpieczną wyprawę po największy skarb, legendarną Koronę Władzy. Wcielisz się w wojownika, kapłana, czarnoksiężnika lub jednego z pozostałych jedenastu bohaterów władających magią lub mieczem. ", USER_2);
+        BoardGame boardGame5 = BoardGame.of(USER_2, "Robinson Crusoe", "Tym razem porywamy was na bezludną wyspę i rzucamy w wir niezwyklych przygód, które przytrafią się grupie rozbitków!", "Dom");
+        BoardGame boardGame6 = BoardGame.of(USER_1, "Magia i Miecz", "W tej pełnej przygód grze wyruszysz na niebezpieczną wyprawę po największy skarb, legendarną Koronę Władzy. Wcielisz się w wojownika, kapłana, czarnoksiężnika lub jednego z pozostałych jedenastu bohaterów władających magią lub mieczem. ", USER_2);
+        BoardGame boardGame7 = BoardGame.of(USER_2, "Odbudowa Warszawy", "Odbudowa Warszawy 1945-1980 to wyjątkowa gra o prostych zasadach, która zapewni dobrą zabawę całej rodzinie, a jej wariant zaawansowany spodoba się również doświadczonym graczom.", "Dom");
+        BoardGame boardGame8 = BoardGame.of(USER_2, "Dobble Kids", "Podstawowa zasada gry to znalezienie jak najszybciej symbolu, który łączy dwie karty. Symbol zawsze jest tylko jeden, o czym należy pamiętać i w to nie wątpić, bo nie raz się zdarza, że nijak nie można go zauważyć.", "Dom");
+        BoardGame boardGame9 = BoardGame.of(USER_2, "Osadnicy z Catanu", "Gracze są osadnikami na niedawno odkrytej wyspie Catan. Każdy z nich przewodzi świeżo założonej kolonii i rozbudowuje ją stawiając na dostępnych obszarach nowe drogi i miasta. Każda kolonia zbiera dostępne dobra naturalne.", "Dom");
         boardGameRepository.save(boardGame1);
         boardGameRepository.save(boardGame2);
         boardGameRepository.save(boardGame3);
         boardGameRepository.save(boardGame4);
         boardGameRepository.save(boardGame5);
+        boardGameRepository.save(boardGame6);
+        boardGameRepository.save(boardGame7);
+        boardGameRepository.save(boardGame8);
+        boardGameRepository.save(boardGame9);
     }
 }
 
